8248a3e5c253a38a5787b6f4011920254f4608ed
Geovanni <gmarcoscardoso@gmail.com>
2024-08-04 08:43:00 -0300

    feat: start project

---
+++ modules/dice/d20-roll.mjs
@@ -0,0 +1,27 @@
+/**
+ * A type of Roll specific to a d20-based check, save, or attack roll in the 5e system.
+ * @param {string} formula                       The string formula to parse
+ * @param {object} data                          The data object against which to parse attributes within the formula
+ * @param {object} [options={}]                  Extra optional arguments which describe or modify the D20Roll
+ * @param {number} [options.advantageMode]       What advantage modifier to apply to the roll (none, advantage,
+ *                                               disadvantage)
+ * @param {number} [options.critical]            The value of d20 result which represents a critical success
+ * @param {number} [options.fumble]              The value of d20 result which represents a critical failure
+ * @param {(number)} [options.targetValue]       Assign a target value against which the result of this roll should be
+ *                                               compared
+ * @param {boolean} [options.elvenAccuracy=false]      Allow Elven Accuracy to modify this roll?
+ * @param {boolean} [options.halflingLucky=false]      Allow Halfling Luck to modify this roll?
+ * @param {boolean} [options.reliableTalent=false]     Allow Reliable Talent to modify this roll?
+ */
+
+export default class D20Roll extends Roll {
+  constructor(formula, data, options) {
+    super(formula, data, options);
+  }
+
+  static fromRoll(roll) {
+    const { formula, data, options } = roll;
+    const newRoll = new this(formula, data, options);
+    return Object.assign(newRoll, roll);
+  }
+}
